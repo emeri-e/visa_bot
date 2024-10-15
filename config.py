e968ab9b9a2880d608db11ba7393a5fb00462cae
emeri-e <danielchiemerie21@gmail.com>
2024-10-15 18:15:23 +0100

    proxy update

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class BaseConfig:
     captcha_submit_url = 'https://ita-pak.blsinternational.com/Global/CaptchaPublic/SubmitCaptcha'
 
     # proxy config("http://spoah8u2pq:gkeF46R1z=R3vVummx@pk.smartproxy.com:10001")
-    proxy_user = config('PROXY_USER', default='spoah8u2pq')
-    proxy_password = config('PROXY_PASSWORD', default='gkeF46R1z=R3vVummx')
+    proxy_user = config('PROXY_USER', default='spckd77hx3')
+    proxy_password = config('PROXY_PASSWORD', default='C3EP8mm+sd92qHhnhw')
     proxy_ip = config('PROXY_IP', default='pk.smartproxy.com')
     proxy_port = config('PROXY_PORT', default='10001')
     
